09f8c5a3f0cc643f8f9e0760a97d930f253cd785
tusharrajput001 <devoptushar@gmail.com>
2024-03-09 23:40:17 +0530

    aboutUs and contact added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Hero from './components/Hero/hero'
 import Enroll from './components/Enroll/enroll'
 import Trainers from './components/Trainers/trainers'
 import Membership from './components/Membership/membership'
+import About from './components/AboutUs/about'
+import Contact from './components/Contact/contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +19,8 @@ function App() {
       {/* <Experience/> */}
       <Trainers/>
       <Membership/>
+      <About/>
+      <Contact/>
 
     </>
   )
