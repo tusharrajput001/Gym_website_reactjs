0775520fea561af772b8aaffb80c142d564c50c5
tusharrajput001 <devoptushar@gmail.com>
2024-03-09 19:06:17 +0530

    plans_added

---
+++ src/components/Membership/memCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './memCard.css'
+
+function MemCard({plan_name,plan_desc,price,f1,f2,f3}) {
+  return (
+    <div className='memb-card'>
+
+        <div className='card-layout'>
+            <h1>{plan_name}</h1>
+            <p>{plan_desc}</p>
+            <h2>{price}</h2>
+            <a className='mem-ico'><i class="fa-solid fa-circle-check"></i>  {f1}</a><br/>
+            <a className='mem-ico'><i class="fa-solid fa-circle-check"></i>  {f2}</a><br/>
+            <a className='mem-ico'><i class="fa-solid fa-circle-check"></i>  {f3}</a>
+
+        </div>
+        
+        <div className='memb-btn'>
+            <button>Select Plan</button>
+        </div>
+
+
+        
+    </div>
+  )
+}
+
+export default MemCard
--- src/components/Membership/membership.jsx
+++ src/components/Membership/membership.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './membership.css'
+import MemCard from './memCard'
 
 
 function Membership() {
@@ -8,7 +9,10 @@ function Membership() {
     <div className='mem-head'>
         Membership
     </div>
-    <div className='mem-card'>
+    <div className='mem-con'>
+        <MemCard plan_name={"BASIC"} plan_desc={"A basic and affordable plan."} price={"$10/m"} f1={"Free outfit"} f2={"Personal Trainer"} f3={"Unlimited access"} />
+        <MemCard plan_name={"PRO"} plan_desc={"Pro plan for Professionals."} price={"$30/m"} f1={"Include all basic plan"} f2={"Free beverages"} f3={"Free online workouts"} />
+        <MemCard plan_name={"ELITE"} plan_desc={"Elite plan for Gymrats."} price={"$50/m"} f1={"Include all Elite plan"} f2={"VIP access"} f3={"Sports complex access"} />
 
     </div>    
     </>
