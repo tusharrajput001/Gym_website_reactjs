6bdbcef1191296e0eeda1b17da2d5938946df267
tusharrajput001 <devoptushar@gmail.com>
2024-03-11 17:45:07 +0530

    responsiveness on trainers

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './hero.css';
 import HeroImg from '../../assets/hero.jpg';
 
 export default function Hero() {
+  useEffect(() => {
+    // Add a class to trigger animation after component mounts
+    document.querySelector('.hero-intro').classList.add('slideIn');
+  }, []);
+
   return (
     <>
       <div className='img-container'>
-        <img src={HeroImg} className='img-hero' alt='hero'/>
-
+        <img src={HeroImg} className='img-hero' alt='hero' />
         <div className='hero-intro'>
           <div className='hero-desc'>DISCOVER</div>
           <div className='hero-desc'>YOUR</div>
           <div className='hero-desc'>STRENGTH</div>
         </div>
-
       </div>
-
-
-
-
     </>
   );
 }
