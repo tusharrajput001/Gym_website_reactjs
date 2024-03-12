5f192f0caee11bd20bc92467cfb3243f3b76cb81
tusharrajput001 <devoptushar@gmail.com>
2024-03-12 15:36:12 +0530

    hove effect

--- src/components/Trainers/trainers.jsx
+++ src/components/Trainers/trainers.jsx
@@ -16,7 +16,7 @@ function Trainers() {
     <div className='cards-con'>
         <Trcard tr_img={tr1} tr_name="Certified Trainer" tr_title="Mens Physique" tr_unv = "Rajveer Shishodia" tr_ig="https://www.instagram.com/rajveer_fitness_series/?hl=en" tr_fb="https://www.facebook.com/RajveerFitnessSeries/"/>
         <Trcard tr_img={tr2} tr_name="Certified Trainer" tr_title="Womens Physique" tr_unv = "Miranda Cohen" tr_ig="https://www.instagram.com/mirandacohenfit/" tr_fb="https://www.facebook.com/mirandacohen.official/"/>
-        <Trcard tr_img={tr3} tr_name="Mr. Olympia" tr_title="Bodybuilding Coach" tr_unv = "Chris Bumstead" tr_ig="https://www.instagram.com/cbum/" tr_fb="https://www.facebook.com/Bumstead1995/"/>
+        <Trcard tr_img={tr3} tr_name="Bodybuilding Champion" tr_title="Mr. Olympia 2022" tr_unv = "Chris Bumstead" tr_ig="https://www.instagram.com/cbum/" tr_fb="https://www.facebook.com/Bumstead1995/"/>
     </div>
     </>
 
