1ca3b102dbb74c8211ee9b179f0719736c0b9cd9
tusharrajput001 <devoptushar@gmail.com>
2024-03-17 14:05:14 +0530

    update on form

--- src/components/Form/form.jsx
+++ src/components/Form/form.jsx
@@ -3,25 +3,23 @@ import './form.css'
 
 function Form() {
   return (
-    <>
+    <div className='form-main'>
     <h1 className='reg-form-head'>Gym Membership Registration Form</h1>
     <div><i className="fa-regular fa-address-card fa-4x icon"></i></div>
     <p className='reg-form-p'>Fill out your information to become our Gym member</p>
 
     <form className="reg-form-container">
     <input className="inp-name" placeholder='First Name'></input>
-      <input className="inp-name" placeholder='Last Name'></input>
-    
-      <br/>
+      <input className="inp-name" placeholder='Last Name'></input><br/>
       <input className="inp-flds" placeholder='Mobile No.'></input> <br/>
       <input className="inp-flds" type='email' placeholder='Your Email'></input> <br/>
       <input className="inp-flds" placeholder='What is your Age'></input> <br/>
       <label className='inp-gen'>What is your gender ?</label><br/>
-      <input type='radio' id='female' name='gender'></input>
+      <input className='inp-rad'type='radio' id='female' name='gender'></input>
       <label for="female">Female</label>
-      <input type='radio' id='male' name='gender'></input>
+      <input className='inp-rad'type='radio' id='male' name='gender'></input>
       <label for='male'>Male</label>
-      <input type='radio' id='other' name='gender'></input>
+      <input className='inp-rad'type='radio' id='other' name='gender'></input>
       <label for='other'>Other</label><br/>
       <label>Choose Your jersy colour: </label>
       <input type="color"></input>
@@ -34,7 +32,7 @@ function Form() {
         <option value="Annual">Annual $200</option>
       </select>
     </form>
-    </>
+    </  div>
 )}
 
 export default Form;
