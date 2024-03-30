a37729c82b0437febddfa7bf3c910b84697c5fe0
tusharrajput001 <devoptushar@gmail.com>
2024-03-31 00:45:08 +0530

    scroll to top when new page loads

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
 //components
 import Navbar from './components/Navbar/navbar';
@@ -16,11 +14,18 @@ import Form from './components/Form/form';
 import Footer from './components/Footer/footer';
 import Loading from './components/Loading/loading';
 
-
 // css
-import "./index.css"
+import "./index.css";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -32,35 +37,33 @@ function App() {
   }, []);
 
   return (
-    <>
-      <div className='App'>
-        {loading ? (
-          <Loading loading={loading} />
-        )  : (
-          <Router>
-            <div>
-              <Navbar />
-              <Routes>
-                <Route path="/" element={
-                  <>
-                    <Navbar />
-                    <Hero />
-                    <Enroll />
-                    <Trainers />
-                    <Membership />
-                    <Partners />
-                    <Contact />
-                  </>
-                } />
-                <Route path="/About" element={<About />} />
-                <Route path="/Form" element={<Form />} />
-              </Routes>
-              <Footer />
-            </div>
-          </Router>
-        )}
-      </div>
-    </>
+    <div className='App'>
+      {loading ? (
+        <Loading loading={loading} />
+      )  : (
+        <Router>
+          <ScrollToTop /> 
+          <div>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={
+                <>
+                  <Hero />
+                  <Enroll />
+                  <Trainers />
+                  <Membership />
+                  <Partners />
+                  <Contact />
+                </>
+              } />
+              <Route path="/About" element={<About />} />
+              <Route path="/Form" element={<Form />} />
+            </Routes>
+            <Footer />
+          </div>
+        </Router>
+      )}
+    </div>
   );
 }
 
