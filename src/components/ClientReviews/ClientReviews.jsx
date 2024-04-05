213f9440535f2043eed1adcc4945bcf0b35de420
tusharrajput001 <devoptushar@gmail.com>
2024-04-05 14:39:41 +0530

    changes

--- src/components/ClientReviews/ClientReviews.jsx
+++ src/components/ClientReviews/ClientReviews.jsx
@@ -5,12 +5,15 @@ import Review_Card from './Review_Card'
 
 function ClientReviews() {
   return (
+    <div className='bg-rev'>
     <div className='reviews-container'>
-        <Review_Card stars_icon= {<i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"></i></i></i></i></i>} review_desc= {"6 months after joining I lost 7kg and I'm getting stronger."} avatar_link = {"https://xsgames.co/randomusers/avatar.php?g=male"}/>
-        <Review_Card stars_icon= {<i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"></i></i></i></i></i>} review_desc= {"I've gotten so much stronger after joining It's insane. My Deadlift increased bt 50kgs."} avatar_link = {"https://xsgames.co/randomusers/avatar.php?g=female"}/>
+        <Review_Card stars_icon= {<i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"></i></i></i></i></i>} review_desc= {"6 months after joining I lost 7kg and I'm getting stronger and getting more Positivity in Lifestyle"} avatar_link = {"https://xsgames.co/randomusers/avatar.php?g=male"}/>
+        <Review_Card stars_icon= {<i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"></i></i></i></i></i>} review_desc= {"I've gotten so much stronger after joining It's insane. My Deadlift increased to 50kgs."} avatar_link = {"https://xsgames.co/randomusers/avatar.php?g=female"}/>
         <Review_Card stars_icon= {<i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"><i class="fa-solid fa-star"></i></i></i></i></i>} review_desc= {"While I am passionate about health and Fitness this Gym is more like a community to me than anything!"} avatar_link = {"https://xsgames.co/randomusers/avatar.php?g=pixel"}/>
     </div>
+    </div>
   )
+
 }
 
 export default ClientReviews
--- src/components/ClientReviews/Review_Card.jsx
+++ src/components/ClientReviews/Review_Card.jsx
@@ -5,7 +5,7 @@ import "./Review_Card.css"
 function Review_Card({stars_icon,review_desc,avatar_link}) {
   return (
     <div className='rev-Crd'>
-      <div>{stars_icon}</div>
+      <div className='stars'>{stars_icon}</div>
       <p>{review_desc}</p>
       <div className='avatar-con'><img src={avatar_link}/></div>
     </div>
